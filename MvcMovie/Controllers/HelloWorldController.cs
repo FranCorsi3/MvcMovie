b87e815fb533f557f529d1bbfa770ec30aa523f7
Franco Corsi <francocorsi1996@gmail.com>
2025-04-03 15:59:29 -0300

    controlador Helloworld/Welcome

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+    }
+
+}
