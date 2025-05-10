262d35075c164e83dabd0bcfffbb4e7ae86dca4f
Franco Corsi <francocorsi1996@gmail.com>
2025-05-10 18:46:07 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard/Index
+        public IActionResult Index()
+        {
+            ViewBag.WelcomeMessage = "Bienvenido al panel de control.";
+            return View();
+        }
+    }
+}
---
+++ Controllers/MovieController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            var listMovies = new List<Movie>();
+
+            var movie1 = new Movie
+            {
+                Genre = "Terror",
+                Id = 1,
+                Price = 1,
+                ReleaseDate = DateTime.Now,
+                Title = "La noche del terror"
+            };
+            listMovies.Add(movie1);
+
+            var movie2 = new Movie
+            {
+                Genre = "Terror",
+                Id = 2,
+                Price = 1,
+                ReleaseDate = DateTime.Now,
+                Title = "La noche del terror II"
+            };
+            listMovies.Add(movie2);
+
+            return View(listMovies);
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = new Movie
+            {
+                Genre = "Terror",
+                Id = 1,
+                Price = 1,
+                ReleaseDate = DateTime.Now,
+                Title = "La noche del terror"
+            };
+
+            return View(movie);
+        }
+
+        // GET: Movies/Nueva
+        public IActionResult Nueva()
+        {
+            return View();
+        }
+    }
+}
