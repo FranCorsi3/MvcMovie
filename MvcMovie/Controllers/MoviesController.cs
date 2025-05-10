b3b401d9cb156971c90ee20e8b35db2c04519926
Franco Corsi <francocorsi1996@gmail.com>
2025-05-10 11:20:49 -0300

    Semana 7

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MvcMovie.Models;
 using System;
 using System.Collections.Generic;
@@ -8,54 +9,71 @@ namespace MvcMovie.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public async Task<IActionResult> Index()
+        [Authorize]
+        public IActionResult Create()
         {
-            var listMovies = new List<Movie>
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Administrador")]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+            // GET: Movies
+            public async Task<IActionResult> Index()
             {
-                new Movie
+                var listMovies = new List<Movie>
+                {
+                    new Movie
+                    {
+                        Genre = "Terror",
+                        Id = 1,
+                        Price = 1,
+                        ReleaseDate = DateTime.Now,
+                        Title = "La noche del terror"
+                    },
+                    new Movie
+                    {
+                        Genre = "Terror",
+                        Id = 2,
+                        Price = 1,
+                        ReleaseDate = DateTime.Now,
+                        Title = "La noche del terror II"
+                    }
+                };
+
+                return View(listMovies);
+            }
+
+            // GET: Movies/Details/5
+            public async Task<IActionResult> Details(int? id)
+            {
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var movie = new Movie
                 {
                     Genre = "Terror",
                     Id = 1,
                     Price = 1,
                     ReleaseDate = DateTime.Now,
                     Title = "La noche del terror"
-                },
-                new Movie
-                {
-                    Genre = "Terror",
-                    Id = 2,
-                    Price = 1,
-                    ReleaseDate = DateTime.Now,
-                    Title = "La noche del terror II"
-                }
-            };
+                };
 
-            return View(listMovies);
-        }
-
-        // GET: Movies/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
+                return View(movie);
             }
-
-            var movie = new Movie
+            public IActionResult NuevaVista()
             {
-                Genre = "Terror",
-                Id = 1,
-                Price = 1,
-                ReleaseDate = DateTime.Now,
-                Title = "La noche del terror"
-            };
-
-            return View(movie);
-        }
-        public IActionResult NuevaVista()
-        {
-            return View();
-        }
+                return View();
+            }
     }
 }
