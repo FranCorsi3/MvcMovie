c99ce029b8304be9d17172e0cf73cd8175dff1a8
Franco Corsi <francocorsi1996@gmail.com>
2025-05-10 11:04:36 -0300

    Semana 6

---
+++ MvcMovie/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.MensajeBienvenida = "¡Bienvenido al panel de usuario!";
+            return View();
+        }
+    }
+}
